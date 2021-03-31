60ad8d9691a92e0f78e1993078bb5687a251f8ae
Yannis Güdel <me@yannisguedel.ch>
2021-03-31 13:07:24 +0200

    chore: fix install script by fixing check if .env file exists

--- mongo/install.sh
+++ mongo/install.sh
@@ -2,7 +2,7 @@ if [ ! -x "./envsubst" ]; then
     sudo curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-`uname -s`-`uname -m` -o ./envsubst
     sudo chmod +x ./envsubst
 fi
-if [ -f .env ]; then
+if [ ! -f .env ]; then
     echo ".env file not found."
     exit 1
 fi
